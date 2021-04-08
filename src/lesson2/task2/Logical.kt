cb5672d14a518ee64b6afcfbbb72c076791f4484
Vladimir <713641@gmail.com>
2021-04-08 19:27:46 +0300

    Решины все задачи

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.*
 
 /**
  * Пример
@@ -18,7 +19,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = (number / 1000 + (number / 100 % 10)) == (number % 10 + number % 100 / 10)
 
 /**
  * Простая
@@ -27,7 +28,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    x1 == x2 || y1 == y2 || (x1 - x2 == y1 - y2) || (x1 == y2 && x2 == y1)
 
 
 /**
@@ -36,7 +38,41 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+//fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return when {
+        year % 4 != 0 || year % 100 == 0 && year % 400 != 0 -> when {
+            month == 1 -> 31
+            month == 2 -> 28
+            month == 3 -> 31
+            month == 4 -> 30
+            month == 5 -> 31
+            month == 6 -> 30
+            month == 7 -> 31
+            month == 8 -> 31
+            month == 9 -> 30
+            month == 10 -> 31
+            month == 11 -> 30
+            month == 12 -> 31
+            else -> 0
+        }
+        else -> when {
+            month == 1 -> 31
+            month == 2 -> 29
+            month == 3 -> 31
+            month == 4 -> 30
+            month == 5 -> 31
+            month == 6 -> 30
+            month == 7 -> 31
+            month == 8 -> 31
+            month == 9 -> 30
+            month == 10 -> 31
+            month == 11 -> 30
+            month == 12 -> 31
+            else -> 0
+        }
+    }
+}
 
 /**
  * Средняя
@@ -48,7 +84,8 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2)) + r1 <= r2
+
 
 /**
  * Средняя
