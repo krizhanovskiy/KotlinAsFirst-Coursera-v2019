6c0493180759d6100f567bfa4bac43b639d903f2
Vladimir <713641@gmail.com>
2021-04-19 12:48:33 +0300

    Решил задачу times

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -166,7 +166,17 @@ fun center(list: MutableList<Double>): MutableList<Double> {
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.
  */
-fun times(a: List<Int>, b: List<Int>): Int = TODO()
+//fun times(a: List<Int>, b: List<Int>): Int = TODO()
+fun times(a: List<Int>, b: List<Int>): Int {
+    var C: Int = 0
+    var i: Int = 0
+    if (a.size == 0 || b.size == 0) return 0
+    while (i < a.size) {
+        C += a[i] * b[i]
+        i += 1
+    }
+    return C
+}
 
 /**
  * Средняя
